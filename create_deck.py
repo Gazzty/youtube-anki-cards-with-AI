3b5a55874c0c4b2bf6176114067bedf3615f14ed
Gazzty <tomasgustavo99@gmail.com>
2025-09-21 20:52:52 -0300

    Changed file create-anki-cards.py to create_deck.py

---
+++ create_deck.py
@@ -0,0 +1,36 @@
+import genanki
+
+# Create a deck model
+model_fields = ['Expression', 'Reading', 'Meaning', 'Notes']
+model_test = genanki.Model(
+	1182953384,	# This is a unique ID generated with python3 -c "import random; print(random.randrange(1 << 30, 1 << 31))"
+	'Test model',
+	fields=[{'name': field} for field in model_fields],
+	templates=[
+		{
+      'name': '日本語 template',
+      'qfmt': '<div class="center"><span style="font-family: irohamaru mikami; font-size: 50px;">{{Expression}}</span></div>',
+      'afmt': '<div class="card-content center"><div class="center"><span style="font-family: irohamaru mikami; font-size: 50px;">{{Expression}}</span></div><hr id="answer" class="separator" /><span style="font-size: 50px;">{{furigana:Reading}}</span><hr id="answer" class="separator" /><div class="left"><span style="font-size: 30px;">{{furigana:Meaning}}</span></div><hr id="answer" class="separator" /><div class="left"><span style="font-size: 30px;">{{furigana:Notes}}</span></div><div class="card-tags-container"><div class="bottom">{{Tags}}</div></div></div>',
+    },
+	]
+)
+
+# Create a note
+note_test = genanki.Note(
+	model=model_test,
+	fields=['Expression', 'Reading', 'Meaning', 'Notes']
+)
+
+# Create a deck
+deck_test = genanki.Deck(
+	1716667060,
+	'Deck test'
+)
+
+# Add note to deck
+deck_test.add_note(note_test)
+
+# Export deck
+print('Creating deck...')
+genanki.Package(deck_test).write_to_file('test_deck.apkg')
+print('Deck created successfully!')
